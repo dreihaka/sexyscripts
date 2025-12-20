b1c8eb25a14099494842d5aaad3e94870c79e1da
Dreihaka <saskeee15@gmail.com>
2025-12-20 23:12:46 +0200

    Fixed it and added parallel processing (so it doesn't take 10 years to do all pings)

--- ping_sweeper.sh
+++ ping_sweeper.sh
@@ -11,11 +11,12 @@ fi
 echo "Scanning subnet $SUBHUMANNET.0/24"
 
 for IP in {1..254}; do
-    ping -c 1 -W "$SUBHUMANNET.$IP" &> /dev/null
+    (ping -c 1 -W 1 "$SUBHUMANNET.$IP" > /dev/null 2>&1
 
 if [ $? -eq 0 ]; then
     echo "Host found: $SUBHUMANNET.$IP"
 fi
+) &
 
 done
 
